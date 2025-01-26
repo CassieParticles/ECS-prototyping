a50302b749b991797b94507837a0fd0c1846a158
Cassie Parsons <Joseph.Parsons2003@outlook.com>
2025-01-26 19:11:40 +0000

    Forgot to commit main

--- main.cpp
+++ main.cpp
@@ -5,20 +5,12 @@
 #include "ECS/ECS.h"
 #include "ECS/Entity.h"
 
-#include "TestComp.h"
 
 
 
 int main()
 {
-	Entity entityA = ECS::createEntity("Entity A");
-	Entity entityB = ECS::createEntity("Entity B");
-	Entity entityC = ECS::createEntity("Entity C");
-	Entity entityD = ECS::createEntity("Entity D");
 
-	entityB.addComponent<TestComp>(3.14159f);
-
-	std::cout<<ECS::getRegistry()->getComponentArr<TestComp>(nullptr)->getEntity().getComponent<TagComponent>()->name<<'\n';
 
 	return 0;
 }
